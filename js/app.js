7baa4c642573ca5de2b5adc41b686bc61971e2d8
RubertG <rubertgonzalez5@gmail.com>
2022-10-11 12:30:25 -0500

    Paginacion

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 const url = 'https://rickandmortyapi.com/api/character'
 const templateCard = document.getElementById('template-card').content
 const cards = document.getElementById('card-dinamicas')
+let pagina = 1
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetchData()
+    fetchData(url)
 })
 
-const fetchData = async () => {
+const fetchData = async (url) => {
     try {
         loadingData(true)
         const rta = await fetch(url)
@@ -21,6 +22,9 @@ const fetchData = async () => {
 }
 
 const pintarCard = data => {
+    btnAnterior()
+    btnSiguiente()
+
     const fragment = document.createDocumentFragment()
     
     data.results.forEach(element => {
@@ -40,5 +44,43 @@ const loadingData = estado => {
         loading.classList.remove('ocultar')
     } else {
         loading.classList.add('ocultar')
+    }
+}
+
+const btnAnterior = () => {
+    if (pagina === 1) {
+        document.querySelector('#btn-anterior').classList.add('ocultar')
+    } else {
+        document.querySelector('#btn-anterior').classList.remove('ocultar')
+    }
+}
+
+const btnSiguiente = () => {
+    if (pagina === 42) {
+        document.querySelector('#btn-siguiente').classList.add('ocultar')
+    } else {
+        document.querySelector('#btn-siguiente').classList.remove('ocultar')
+    }
+}
+
+const irAnterior = () => {
+    if (pagina != 1) {
+        Limpiar()
+        pagina--
+        fetchData(url + '/?page=' + pagina)
+    }
+}
+
+const irSiguiente = () => {
+    if (pagina != 42) {
+        Limpiar()
+        pagina++
+        fetchData(url + '/?page=' + pagina)
+    }
+}
+
+const Limpiar = () => {
+    for (i = 0; i <= 19; i++){
+        cards.removeChild(document.querySelector('.card'))
     }
 }
